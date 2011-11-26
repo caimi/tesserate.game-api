3d8ab12e28af7e52ecb47d042924d8f74b5a809f
caimi <defiler@desktop.(none)>
2011-11-26 16:28:31 -0200

    double random

--- src/main/java/com/tesserate/game/api/util/Util.java
+++ src/main/java/com/tesserate/game/api/util/Util.java
@@ -9,4 +9,23 @@ public class Util {
 		return (int) Math.rint(Math.random()*(max-min))+min;
 	}
 
+	public static double rnd(double x){
+		return Math.rint(Math.random()*x);
+	}
+	public static double rnd(double min, double max){
+		return Math.rint(Math.random()*(max-min))+min;
+	}
+
+	public static String removeLowerCase(String msg) {
+		String upperCase = "ABCDEFGHIJKLMNOPKRSTUVXWYZ";
+		String result = "";
+		
+		for (int i = 0; i < msg.length(); i++)
+			for (int j = 0; j < upperCase.length(); j++){
+				if(msg.charAt(i)==upperCase.charAt(j)){
+					result += msg.charAt(i);
+				}
+			}
+		return result;
+	}
 }
